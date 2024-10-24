d457a6b87581369da2dbb3ba97dbce9bdc75a1a6
Fillah Alamsyah <87614211+fillahalamsyah@users.noreply.github.com>
2024-10-24 16:39:34 +0000

    Add yml file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    // ... other config
+    site: 'https://fillahalamsyah.github.io',
+    base: 'fillahalamsyah.github.io',
     integrations: [],
 });
